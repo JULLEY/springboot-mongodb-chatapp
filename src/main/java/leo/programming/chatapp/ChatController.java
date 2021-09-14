8201da890f68d75d2e8500115e1adb8cccdf4667
woong <ung91@yahoo.com>
2021-09-14 22:12:22 +0900

    주석 강화

--- src/main/java/leo/programming/chatapp/ChatController.java
+++ src/main/java/leo/programming/chatapp/ChatController.java
@@ -12,14 +12,17 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @RestController // 데이터 리턴 서버
 public class ChatController {
+
     private final ChatRepository chatRepository;
 
+    // 메세지 수신
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE) // 데이터가 지속적으로 흐르게 해준다
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver){
         return chatRepository.mFindBySender(sender, receiver)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
+    // 메세지 송신
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat){
         chat.setCreatedAt(LocalDateTime.now());
